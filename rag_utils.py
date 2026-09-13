b3e76080e0d25c0bf18b7136a4d2b6f2fb255c76
nitingwd <nitinkadiya445@gmail.com>
2026-09-13 15:42:29 +0530

    Refactor rag_utils.py to update imports and logic

--- rag_utils.py
+++ rag_utils.py
@@ -2,7 +2,7 @@ import os
 import streamlit as st
 import tempfile
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader, CSVLoader, TextLoader
